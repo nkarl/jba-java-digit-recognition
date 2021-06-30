71564a6015dd111213ba0d72a4cfffd2d8354acf
nkarl <nordenk@pme.me>
2021-06-29 19:04:40 -0700

    finished milestone-1

--- Topics/Multidimensional array/Fill the matrix by numbers/src/Main.java
+++ Topics/Multidimensional array/Fill the matrix by numbers/src/Main.java
@@ -6,8 +6,21 @@ class Main {
         Scanner scanner = new Scanner(System.in);
         int size = scanner.nextInt();
         int[][] arr = new int[size][size];
-//        for (int i=1; i < size - 1; ++i) {
-//            for
-//        }
+        for (int i = 0; i < size; ++i) {
+            int seed = 0;
+            for (int j = i; j < size; ++j) {
+                arr[i][j] = seed;
+                arr[j][i] = seed;
+                ++seed;
+            }
+        }
+
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                System.out.print(arr[i][j]);
+                System.out.print(' ');
+            }
+            System.out.print('\n');
+        }
     }
 }
