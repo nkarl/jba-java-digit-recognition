667ee15e367d1eae559a01744c85c9e8bde1226c
nkarl <nordenk@pme.me>
2021-06-29 18:19:08 -0700

    added more 2D array practice

--- Topics/Multidimensional array/Fill the matrix by numbers/src/Main.java
+++ Topics/Multidimensional array/Fill the matrix by numbers/src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
         // put your code here
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int[][] arr = new int[size][size];
+//        for (int i=1; i < size - 1; ++i) {
+//            for
+//        }
     }
 }
