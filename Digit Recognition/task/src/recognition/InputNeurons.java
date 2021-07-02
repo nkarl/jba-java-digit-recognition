e89dc5c4748bfc2842423940ea854f1d97488444
nkarl <nordenk@pme.me>
2021-07-02 16:46:00 -0700

    separated and encapsulated the input neurons into their only class

--- Digit Recognition/task/src/recognition/InputNeurons.java
+++ Digit Recognition/task/src/recognition/InputNeurons.java
@@ -1,15 +1,24 @@
 package recognition;
 
-public class InputNeurons {
-    public final short WHITE = -1;
-    public final short BLUE = 1;
-    public final int Rs = 3;
-    public final int Cs = 5;
+import java.util.Scanner;
 
-    public int[][] w ;
+public class InputNeurons {
+    public final int WHITE = -1;
+    public final int BLUE = 1;
+    public final int r_dim = 5;
+    public final int c_dim = 3;
+    public int[][] inNeurons;  // the input cells
 
     public InputNeurons() {
-        this.w = new int[Rs][Cs];
-
+        Scanner scanner = new Scanner(System.in);
+        this.inNeurons = new int[r_dim][c_dim];  // instantiate a new RW x CL matrix
+        // map the raw string to the input cells
+        for (int i = 0; i < r_dim; ++i) {
+            String line = scanner.nextLine();
+            for (int j = 0; j < c_dim; ++j) {
+                this.inNeurons[i][j] = (line.charAt(j) == '_') ? WHITE : BLUE;
+            }
+            scanner.close();
+        }
     }
 }
--- Digit Recognition/task/src/recognition/Main.java
+++ Digit Recognition/task/src/recognition/Main.java
@@ -1,32 +1,20 @@
 package recognition;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        // write your code here
-        Scanner scanner = new Scanner(System.in);
-        int size = 3;
-        var bitmap = new int[size][size];
-        for (int i = 0; i < size; ++i) {
-            String line = scanner.nextLine();
-            for (int j = 0; j < size; ++j) {
-                bitmap[i][j] = (line.charAt(j) == '_') ? -1 : 1;
-            }
-        }
-    
-        OutputNeurons net = new OutputNeurons();
-        int neuron = 0;
-        for (int i = 0; i < size; ++i) {
-            for (int j = 0; j < size; ++j) {
-                neuron += (bitmap[i][j] * net.w[i][j]);
-            }
-        }
-        neuron += net.b;
+        InputNeurons net = new InputNeurons();
 
-        int result = (neuron >= 0) ? 0 : 1;
-
-        System.out.println("This number is " + result);
-        scanner.close();
+//        OutputNeurons net = new OutputNeurons();
+//        int neuron = 0;
+//        for (int i = 0; i < size; ++i) {
+//            for (int j = 0; j < size; ++j) {
+//                neuron += (bitmap[i][j] * net.w[i][j]);
+//            }
+//        }
+//        neuron += net.b;
+//
+//        int result = (neuron >= 0) ? 0 : 1;
+//
+//        System.out.println("This number is " + result);
     }
 }
