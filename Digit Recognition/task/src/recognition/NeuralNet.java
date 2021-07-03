eec28ad7c3f3cf3b6d8b338969d1f604feb05f80
nkarl <nordenk@pme.me>
2021-07-02 19:35:22 -0700

    added a function to initialize weighted map for each digit

--- Digit Recognition/task/src/recognition/NeuralNet.java
+++ Digit Recognition/task/src/recognition/NeuralNet.java
@@ -2,23 +2,20 @@ package recognition;
 
 import java.util.Scanner;
 
-public class NeuralNet {
-    public final int WHITE = -1;
-    public final int BLUE = 1;
-    public final int r_dim = 5;
-    public final int c_dim = 3;
-    public int[][] weighted_inCells;  // the input cells
+public class NeuralNet extends DigitMap {
+    public int[][] inputCells;
     public int[] outCells;
     public int[] biases;
 
     public NeuralNet() {
+        this.inputCells= this.map;
+
         Scanner scanner = new Scanner(System.in);
-        this.weighted_inCells = new int[r_dim][c_dim];  // instantiate a new RW x CL matrix
         // map the raw string to the weighted (edge) input cells
         for (int i = 0; i < r_dim; ++i) {
             String line = scanner.nextLine();
             for (int j = 0; j < c_dim; ++j) {
-                this.weighted_inCells[i][j] = (line.charAt(j) == '_') ? WHITE : BLUE;
+                this.inputCells[i][j] = (line.charAt(j) == '_') ? 0 : 1;
             }
         }
         this.outCells = new int[10];  // for the 10 digits
